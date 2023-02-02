fc8909e86e92756e4ae91ef45b92cd9f1d4fd405
UnnamedMistress <momchrysti@gmail.com>
2023-02-02 17:37:54 -0500

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,25 +1,33 @@
-import { Configuration, OpenAIApi } from "openai";
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const OpenAIApi = require("openai").OpenAIApi;
+const Configuration = require("openai").Configuration;
+require("dotenv").config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-CjIl4vnLKCaPqr6WGtIhrpQA",
+  apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
+app.use(bodyParser.json());
+
+app.post("/generate-animal-names", async (req, res) => {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
+        message: "OpenAI API key not configured, please follow instructions in README.md"
       }
     });
     return;
   }
 
-  const animal = req.body.animal || '';
+  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid animal"
       }
     });
     return;
@@ -29,11 +37,10 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 0.6,
+      temperature: 0.6
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -41,12 +48,12 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
+          message: "An error occurred during your request."
         }
       });
     }
   }
-}
+});
 
 function generatePrompt(animal) {
   const capitalizedAnimal =
@@ -60,3 +67,8 @@ Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
 Names:`;
 }
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
